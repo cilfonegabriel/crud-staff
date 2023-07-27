c3cffc509a3ec08881c94e1bcc3de104bfabf9a3
gabicilfone <cilfonegabriel@gmail.com>
2023-07-27 11:01:55 -0300

    changes on delete function

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,32 @@
 import './App.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Swal from 'sweetalert2'
 
-
 function App() {
-
   const [name, setName] = useState("");
-  const [age, setAge] = useState();
+  const [age, setAge] = useState('');
   const [country, setCountry] = useState("");
   const [post, setPost] = useState("");
-  const [years, setYears] = useState();
-  const [id, setId] = useState();
-
-
+  const [years, setYears] = useState(0);
+  const [id, setId] = useState("");
   const [edit, setEdit] = useState(false);
-
   const [employeesList, setEmployees] = useState([]);
 
+  useEffect(() => {
+    getEmployees(); // Fetch employees when the component mounts
+  }, []);
+
   const add = () => {
-    Axios.post('http://localhost:3001/create',{
+    Axios.post('http://localhost:3001/create', {
       name: name,
       age: age,
       country: country,
       post: post,
       years: years
-    }).then(() => {
+    })
+    .then(() => {
       getEmployees();
       clearnFields();
       Swal.fire({
@@ -35,38 +34,43 @@ function App() {
         html: "<i>The employee <strong>"+ name + "</strong> was loaded successfully!</i>",
         icon: 'success',
         timer: 3000
-      }).catch(function(error){
-        Swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: JSON.parse(JSON.stringify(error)).message==="Network error"?"Try later":JSON.parse(JSON.stringify(error)).message
-        })
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'An error occurred while adding the employee. Please try again later.',
       });
     });
   }
 
   const update = () => {
-    Axios.put('http://localhost:3001/update',{
+    Axios.put('http://localhost:3001/update', {
       id: id,
       name: name,
       age: age,
       country: country,
       post: post,
       years: years
-    }).then(() => {
+    })
+    .then(() => {
       getEmployees();
       clearnFields();
       Swal.fire({
-        title:"<strong> successful update! </strong>",
+        title:"<strong> Successful update! </strong>",
         html: "<i>The employee <strong>"+ name + "</strong> was updated  successfully!</i>",
         icon: 'success',
         timer: 3000
-      }).catch(function(error){
-        Swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: JSON.parse(JSON.stringify(error)).message==="Network error"?"Try later":JSON.parse(JSON.stringify(error)).message
-        })
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'An error occurred while updating the employee. Please try again later.',
       });
     });
   }
@@ -80,26 +84,27 @@ function App() {
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
       confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
+    })
+    .then((result) => {
       if (result.isConfirmed) {
-        Axios.delete(`http://localhost:3001/delete/${val.id}`,{}).then(() => {
+        Axios.delete(`http://localhost:3001/delete/${val.id}`, {})
+        .then(() => {
           getEmployees();
           clearnFields();
-          Swal.fire(
-            {
-              icon: 'success',
-              title: val.name+' was removed.',
-              showConfirmButton: false,
-              timer: 2000
-            }
-          );
-        }).catch(function(error){
+          Swal.fire({
+            icon: 'success',
+            title: val.name+' was removed.',
+            showConfirmButton: false,
+            timer: 2000
+          });
+        })
+        .catch((error) => {
+          console.log(error);
           Swal.fire({
             icon: 'error',
             title: 'Oops...',
-            text: 'Could not delete!',
-            footer: JSON.parse(JSON.stringify(error)).message==="Network error"?"Try later":JSON.parse(JSON.stringify(error)).message
-          })
+            text: 'An error occurred while deleting the employee. Please try again later.',
+          });
         });
       }
     });
@@ -117,7 +122,6 @@ function App() {
 
   const editEmployye = (val) => {
     setEdit(true);
-
     setName(val.name);
     setAge(val.age);
     setPost(val.post);
@@ -125,17 +129,22 @@ function App() {
     setName(val.name);
     setYears(val.years);
     setId(val.id);
-
-
-}
+  }
 
   const getEmployees = () => {
-    Axios.get('http://localhost:3001/employees').then((response) => {
+    Axios.get('http://localhost:3001/employees')
+    .then((response) => {
       setEmployees(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'An error occurred while fetching employees. Please try again later.',
+      });
     });
   };
-
-  getEmployees();
 
   return (
     <div className="container">
